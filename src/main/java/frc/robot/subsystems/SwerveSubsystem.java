ca752e0e8f56790d6f8e7d9881fb7b86900b3eae
Nobody <149746398+nobodyhere4@users.noreply.github.com>
2026-01-13 16:38:50 -0600

    Added vendors

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -2,6 +2,6 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class DriveTrainSubsystem extends SubsystemBase {
+public class SwerveSubsystem extends SubsystemBase {
 
 }
