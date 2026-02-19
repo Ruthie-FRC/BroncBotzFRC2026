c6e09259ccb7744dcc049cc873ceebe1e4dad9b0
thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>
2026-02-19 09:59:10 -0600

    Fixed sim.

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -165,6 +165,7 @@ public class SwerveSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    swerveDrive.updateOdometry();
     limelight
         .getSettings()
         .withRobotOrientation(
