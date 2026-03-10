45f30f296a946fa46eee6e853fa0aa75093351da
Nobody <149746398+nobodyhere4@users.noreply.github.com>
2026-03-10 09:18:55 -0500

    reset setpoints

--- src/main/java/frc/robot/Setpoints.java
+++ src/main/java/frc/robot/Setpoints.java
@@ -15,10 +15,10 @@ public class Setpoints {
         public static final AngularVelocity intakeRollerRPM = RPM.of(-1000);
         public static final AngularVelocity outtakeRollerRPM = RPM.of(1000);
         public static final Angle           intakeArmStartAngle = Degrees.of(0); // change back to 45
-        public static final Angle           intakeArmAngleUp    = Degrees.of(5);
+        public static final Angle           intakeArmAngleUp    = Degrees.of(45);
         public static final AngularVelocity agitatorRPMout      = RPM.of(-500);
         public static final AngularVelocity agitatorRPMin      = RPM.of(500);
-        public static Angle                 intakeArmAngleDown = Degrees.of(45);
+        public static Angle                 intakeArmAngleDown = Degrees.of(0);
 
       public static Angle intakeArmAbsZeroOffset = Degrees.of(0);
     }
